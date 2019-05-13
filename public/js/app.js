684c10eefc61b93597969804cea33ec6e3a6d326
prescottbreeden <prescott.breeden@gmail.com>
2019-05-12 18:10:14 -0700

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+// window.onload = () => {
+// 	const canvas = document.getElementById('cid');
+// 	const context = canvas.getContext('2d');
+// 	const WIDTH = window.innerHeight;
+// 	const HEIGHT = window.innerWidth;
+// 	const radian = Math.PI/180;
+// 	canvas.width = WIDTH;
+// 	canvas.height = HEIGHT;
+
+// }
